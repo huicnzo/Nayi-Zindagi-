e9c0c3a61a81b1b20468f31dd34eb2251926fadb
mahiiiiemaaaa <73066217+huicnzo@users.noreply.github.com>
2021-02-28 12:16:56 +0530

    Create nlp-1.py

---
+++ NLP/nlp-1.py
@@ -0,0 +1,3 @@
+# Computers can understand the structured form of data like spreadsheets and the tables in the database, 
+#but human languages, texts, and voices form an unstructured category of data, and it gets difficult for the 
+#computer to understand it, and there arises the need for Natural Language Processing.
